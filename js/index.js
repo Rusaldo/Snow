efe74bbea34635f401a1b10938fff752dc527e63
ruslan <yandiev_97@mail.ru>
2018-02-05 19:29:26 +0300

    Projects started

--- js/index.js
+++ js/index.js
@@ -5,13 +5,18 @@ $(document).ready(function() {
     document.querySelector(".menu__list").classList.toggle("active");
     document.querySelector(".menu__toggle").classList.toggle("active");
   };
+  
+  $(".menu__link").click(function() {
+    $(".menu__list").removeClass("active");
+    $(".menu__toggle").removeClass("active");
+  });
 
   // HEADER FIX ON SCROLL
+  var header = document.getElementById("header");
+  var hero = document.getElementById("hero");
+
   window.onscroll = function() {
-    var header = document.getElementById("header");
-    var hero = document.getElementById("hero");
-    var heroHeight = (window.getComputedStyle(hero, null).height).replace(/\D/g,'');
-    if (window.pageYOffset >= heroHeight) {
+    if (window.pageYOffset >= (window.getComputedStyle(hero, null).height).replace(/\D/g,'') -20) {
       header.classList.add("fixed");
     } else {
       header.classList.remove("fixed");
