88d8b74977e8885b7966521467b58f6a469878eb
ruslan <yandiev_97@mail.ru>
2018-02-19 21:27:48 +0300

    Preloader and header fixed

--- js/index.js
+++ js/index.js
@@ -74,12 +74,46 @@ $(document).ready(function() {
   });
 
   // HEADER FIX ON SCROLL
-  window.onscroll = function() {
-    if (window.pageYOffset >= (window.getComputedStyle(hero, null).height).replace(/\D/g,'') -20) {
-      $("header").addClass("fixed");
-    } else {
-      $("header").removeClass("fixed");
+  $(window).scroll(function() {
+    var heroHeight = window.getComputedStyle(document.querySelector(".hero")).height;
+    var heroHeightNum = Math.floor(Number(heroHeight.slice(0, heroHeight.length - 2)));
+
+    if ($(window).scrollTop() >= heroHeightNum - 50) {
+       $(".header").addClass("fixed");
     }
-  };
+    else {
+       $(".header").removeClass("fixed");
+    }
+  });
+
+  // SMOOTH SCROLL EFFECT
+  $('a[href*="#"]')
+ 	.not('[href="#"]')
+  	.not('[href="#0"]')
+  	.click(function(event) {
+    if (
+    	location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+    )   {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000, function() {
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+         	   return false;
+            } else {
+           	 $target.attr('tabindex','-1');
+           	 $target.focus();
+            };
+        	});
+     		}
+    	}
+    });
 
 });
